64ca0979803ca0e43c2fa6571e8aca82c26e8ed3
Denali Balser <denali.balser167@gmail.com>
2019-10-09 18:55:08 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,3 +1,19 @@
-class SuperHero < ActiveRecord::Base
-  
+class SuperHero
+  attr_reader :name, :power, :bio
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    self.save
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
